9088117882ed0121fe26d1aab10d7a49894b5eb5
leedohun <33706689+leedohun@users.noreply.github.com>
2021-04-23 16:04:41 +0900

    Update az_win_agent.ps1

--- azure_win/az_win_agent.ps1
+++ azure_win/az_win_agent.ps1
@@ -1,16 +1,1 @@
-$ZCM_IP="www.z-cloud.net"
-$USER="admin@zconverter.com"
-$ZCM_IPaddress="150.136.194.83"
-
-$dir=echo $env:temp
-
-ipconfig > $dir/result.txt
-ipconfig > c:\reuslt.txt
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-
-Invoke-WebRequest -Uri http://$ZCM_IP/Download/ZConverter_CloudTargetClient_Setup_last_1.exe -Outfile $dir/agent.exe
-powershell.exe -command "$dir/agent.exe /zcm $ZCM_IPaddress /user $USER"
-Invoke-WebRequest -Uri http://$ZCM_IP/Download/WinInitScript.cmd -Outfile $dir/WinInitScript.cmd
-powershell.exe -command "cmd.exe /c $dir/WinInitScript.cmd"
+ipconfig > c:\result.txt
