964add5fb3f1280389d6b0253a7fe4b066174dec
leedohun <33706689+leedohun@users.noreply.github.com>
2021-04-23 15:59:49 +0900

    Update az_linux_agent.sh

--- azure_linux/az_linux_agent.sh
+++ azure_linux/az_linux_agent.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-yum install wget -y
-
-ZCM_IP=150.136.194.83
-USER=admin@zconverter.com
-
-wget -P /tmp/ --tries=200 http://${ZCM_IP}/Download/ZConverter_CloudTargetClient_Setup_V3.4_Build_3073.tar.gz &> /tmp/wget.log --no-check-certificate
-sleep 10
-tar zxvf /tmp/ZConverter_CloudTargetClient_Setup_V3.4_Build_3073.tar.gz -C /tmp
-/tmp/zconverter_install_target/install.sh default2 ${ZCM_IP} ${USER} &
-
+ifconfig >> /tmp/result
