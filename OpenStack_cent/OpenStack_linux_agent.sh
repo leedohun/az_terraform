c942ff65dfe64dc582cd256580d4e4de254a1104
leedohun <33706689+leedohun@users.noreply.github.com>
2021-04-25 07:16:19 +0900

    Update OpenStack_linux_agent.sh

--- OpenStack_cent/OpenStack_linux_agent.sh
+++ OpenStack_cent/OpenStack_linux_agent.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-echo nameserver 8.8.8.8 >> /etc/resolv.conf
-
-yum install wget -y
-
-ZCM_IP=150.136.194.83
-USER=admin@zconverter.com
-
-wget -P /tmp/ --tries=200 http://${ZCM_IP}/Download/ZConverter_CloudTargetClient_Setup_V3.4_Build_3073.tar.gz &> /tmp/wget.log --no-check-certificate
-sleep 10
-tar zxvf /tmp/ZConverter_CloudTargetClient_Setup_V3.4_Build_3073.tar.gz -C /tmp
-/tmp/zconverter_install_target/install.sh default2 ${ZCM_IP} ${USER} &
-
+ipconfig >> /tmp/result.txt
