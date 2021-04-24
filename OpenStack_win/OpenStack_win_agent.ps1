48351624921205af7a622f1097162cca4007aacf
leedohun <33706689+leedohun@users.noreply.github.com>
2021-04-25 07:14:43 +0900

    Update OpenStack_win_agent.ps1

--- OpenStack_win/OpenStack_win_agent.ps1
+++ OpenStack_win/OpenStack_win_agent.ps1
@@ -1,15 +1,3 @@
 #ps1
 
-$ZCM_IP="www.z-cloud.net"
-$USER="admin@zconverter.com"
-$ZCM_IPaddress="150.136.194.83"
-
-$dir="C:\windows\Temp"
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-
-Invoke-WebRequest -Uri http://$ZCM_IP/Download/ZConverter_CloudTargetClient_Setup_last_1.exe -Outfile $dir\agent.exe
-powershell.exe -command "$dir/agent.exe /zcm $ZCM_IPaddress /user $USER"
-Invoke-WebRequest -Uri http://$ZCM_IP/Download/WinInitScript.cmd -Outfile $dir\WinInitScript.cmd
-powershell.exe -command "cmd.exe /c $dir/WinInitScript.cmd"
+ipconfig >> c:\result.txt
